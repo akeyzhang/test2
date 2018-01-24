b790a974a2bdb4314d25662ba67b0b8b3e162e40
akey <akeyzhang@126.com>
2018-01-24 17:03:44 +0800

    20180124_16:54

--- js/akeyfunction.js
+++ js/akeyfunction.js
@@ -13,3 +13,10 @@ function getCurrentDate() {
     return currentdate;
 
 }
+
+function guid() {
+    return 'bbxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+        return v.toString(16);
+    });
+}
