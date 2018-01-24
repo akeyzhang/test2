7c1118b208df6dc223d4c70bfa52e05e20e036ab
akeyzhang <akeyzhang@126.com>
2018-01-24 23:08:51 +0800

    20180124_22:01

--- js/akeyfunction.js
+++ js/akeyfunction.js
@@ -15,7 +15,7 @@ function getCurrentDate() {
 }
 
 function guid() {
-    return 'bbxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    return 'bkxxxxxxxxxx'.replace(/[xy]/g, function(c) {
         var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
         return v.toString(16);
     });
