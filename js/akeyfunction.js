86ba41350e83a2ff80509d689ba53a9268864080
akey <akeyzhang@126.com>
2018-01-27 10:48:50 +0800

    20180127_10:58

--- js/akeyfunction.js
+++ js/akeyfunction.js
@@ -16,6 +16,10 @@ function getCurrentDate() {
 
 }
 
+function getCurrentDateTime() {
+    return new Date().toLocaleString()
+}
+
 function guid() {
     return 'bkxxxxxxxxxx'.replace(/[xy]/g, function(c) {
         var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
