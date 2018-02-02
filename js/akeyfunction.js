8f3ca327a555d2194ec4d9a57330033088f396f2
akey <akeyzhang@126.com>
2018-02-02 14:33:49 +0800

    20180202_15:55

--- js/akeyfunction.js
+++ js/akeyfunction.js
@@ -21,6 +21,11 @@ function getCurrentDateTime() {
     return new Date().toLocaleString()
 }
 
+function getCurrentFormatDateTime(formatstr) {
+    return FormatDateTimeStr(new Date(),formatstr);
+
+}
+
 function guid() {
     return 'bkxxxxxxxxxx'.replace(/[xy]/g, function(c) {
         var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
@@ -31,4 +36,10 @@ function guid() {
 //去除掉字串中的T00:00:00Z時區信息,一般用在數據庫返回的日期類型.
 function trimDateTimeZone(str) {
     return str.replace(new RegExp("T00:00:00Z","gm"),"");
+}
+
+function FormatDateTimeStr(x, y) {  //x--要處理的日期, y--日期格式模板.
+    var z = { M: x.getMonth() + 1, d: x.getDate(), h: x.getHours(), m: x.getMinutes(), s: x.getSeconds() };
+    y = y.replace(/(M+|d+|h+|m+|s+)/g, function (v) { return ((v.length > 1 ? "0" : "") + eval('z.' + v.slice(-1))).slice(-2) });
+    return y.replace(/(y+)/g, function (v) { return x.getFullYear().toString().slice(-v.length) });
 }
